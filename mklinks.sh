e395ba4bebdee0bbb1578b70708806b7a46ef594
critzlez <jm.armstrong1207@gmail.com>
2019-10-20 18:48:45 -0700

    remove x files

--- mklinks.sh
+++ mklinks.sh
@@ -6,6 +6,3 @@ ln zshrc 	   ~/.zshrc
 ln oh-my-zsh-custom/themes/agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/
 ln oh-my-zsh-custom/themes/example.zsh-theme  ~/.oh-my-zsh/custom/themes/
 ln oh-my-zsh-custom/themes/mh.zsh-theme       ~/.oh-my-zsh/custom/themes/
-
-ln xinitrc  ~/.xinitrc
-ln xprofile ~/.xprofile
