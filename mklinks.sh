40a00153c5be6229f52d4b0c730857a49a8ba5c7
critzlez <jm.armstrong1207@gmail.com>
2020-03-10 16:22:45 -0700

    alias xflock4 with betterlockscreen

--- mklinks.sh
+++ mklinks.sh
@@ -5,6 +5,9 @@ ln init.vim 	   ~/.config/nvim/
 ln zshrc 	   ~/.zshrc
 ln i3config ~/.config/i3/config
 
+
 ln oh-my-zsh-custom/themes/agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/
 ln oh-my-zsh-custom/themes/example.zsh-theme  ~/.oh-my-zsh/custom/themes/
 ln oh-my-zsh-custom/themes/mh.zsh-theme       ~/.oh-my-zsh/custom/themes/
+
+sudo ln xflock4 /usr/local/bin/
